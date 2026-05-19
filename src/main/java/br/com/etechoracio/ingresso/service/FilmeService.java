ebb484f21e0f742936bc2f2fd1da6130c5bfe251
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-05-19 08:57:02 -0300

    Código concluído - aula 19/05

--- src/main/java/br/com/etechoracio/ingresso/service/FilmeService.java
+++ src/main/java/br/com/etechoracio/ingresso/service/FilmeService.java
@@ -3,6 +3,7 @@ package br.com.etechoracio.ingresso.service;
 import br.com.etechoracio.ingresso.dto.FilmeResponseDTO;
 import br.com.etechoracio.ingresso.entity.Filme;
 import br.com.etechoracio.ingresso.enums.SimNaoEnum;
+import br.com.etechoracio.ingresso.mapper.FilmeMapper;
 import br.com.etechoracio.ingresso.repository.FilmeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +15,22 @@ import java.util.List;
 public class FilmeService {
 
     @Autowired
+    private FilmeMapper filmeMapper;
+
+    @Autowired
     private FilmeRepository filmeRepository;
 
+//    public List</*Filme*/ FilmeResponseDTO> findAll(){
+//        var filmes = filmeRepository.findAll();
+//        return null;
+//    }
     public List</*Filme*/ FilmeResponseDTO> findAll(){
         var filmes = filmeRepository.findAll();
-        return null;
+        return filmeMapper.toResponseDTOList(filmes);
     }
 
     public List</*Filme*/ FilmeResponseDTO> listarEmCartaz() {
         var filmes = filmeRepository.findByEmCartaz(SimNaoEnum.S);
-        return null;
+        return filmeMapper.toResponseDTOList(filmes);
     }
 }
