5e92375ed8d218cd917fa9aa6da43f9d1f48aa52
Rafael Martins Nogueira <rafamartnogueira@gmail.com>
2026-06-02 08:57:17 -0300

    API, backend - 02/06

--- src/main/java/br/com/etechoracio/ingresso/controller/FilmeController.java
+++ src/main/java/br/com/etechoracio/ingresso/controller/FilmeController.java
@@ -5,12 +5,14 @@ import br.com.etechoracio.ingresso.entity.Filme;
 import br.com.etechoracio.ingresso.service.FilmeService;
 import org.apache.logging.log4j.message.FlowMessage;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/filmes")
 public class FilmeController {
